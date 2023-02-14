ae711b99c62d522f618e3a75aad6486a2e1e1f7d
Vlad <vladislav.stan.1998@gmail.com>
2023-02-14 14:47:42 +0200

    create and added tabs

--- js/script.js
+++ js/script.js
@@ -1,2 +1,42 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          contents = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideContent() {
+        contents.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showContent(i = 0) {
+        contents[i].style.display = 'block';
+
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (item == target) {
+
+                    hideContent();
+                    showContent(i);
+                }
+            });
+        }
+    })
 
 
+    hideContent();
+    showContent();
+});
